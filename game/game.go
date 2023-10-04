88e454200d5d98ae30c4c8faad8678f971551bd1
Me <me@localhost>
2023-10-04 22:49:31 +0300

    touch recognition added

--- game/game.go
+++ game/game.go
@@ -2,11 +2,11 @@ package game
 
 import (
 	"fmt"
+	"image"
 	"image/color"
 	"image/png"
 	_ "image/png"
 	"math/rand"
-	"os"
 	"bytes"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
@@ -14,7 +14,19 @@ import (
 	"embed"
 )
 
+const (
+	screenWidth  = 320
+	screenHeight = 340
+
+	frameOX     = 0
+	frameOY     = 0
+	frameWidth  = 84
+	frameHeight = 250
+	frameCount  = 15
+)
+
 //go:embed bubble.png
+//go:embed weed.png
 var f embed.FS
 
 type Matrix struct {
@@ -70,41 +82,45 @@ type Game struct {
 	freq int
 	over bool
 	bubbleImage *ebiten.Image
+	weedImage *ebiten.Image
 }
 
-func NewGame() *Game {
+// func NewGame() *Game {
 
-	data, err := os.ReadFile("bubble.png")
-	if err != nil {
-		fmt.Println(err)
-	}
-	imageReader := bytes.NewReader(data)
-	image, err := png.Decode(imageReader)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return &Game{
-		field: &Matrix{
-			cells:make([]int, 10*20),
-			width:10,
-			height:20,			
-		}, 
-		cell_size: 10,	
-		freq:10,
-		over:false,
-		bubbleImage:ebiten.NewImageFromImage(image),
-	}
-}
+// 	data, err := os.ReadFile("bubble.png")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	imageReader := bytes.NewReader(data)
+// 	image, err := png.Decode(imageReader)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	return &Game{
+// 		field: &Matrix{
+// 			cells:make([]int, 10*20),
+// 			width:10,
+// 			height:20,			
+// 		}, 
+// 		cell_size: 10,	
+// 		freq:10,
+// 		over:false,
+// 		bubbleImage:ebiten.NewImageFromImage(image),
+// 	}
+// }
 
 func (g *Game) Init() {
 
-	data, err := f.ReadFile("bubble.png")
-	//data, err := os.ReadFile("bubble.png")
+	data, _ := f.ReadFile("bubble.png")
+	imageReader := bytes.NewReader(data)
+	image, _ := png.Decode(imageReader)
+
+	data, err := f.ReadFile("weed.png")
 	if err != nil {
 		fmt.Println(err)
 	}
-	imageReader := bytes.NewReader(data)
-	image, err := png.Decode(imageReader)
+	imageReader1 := bytes.NewReader(data)
+	img, err := png.Decode(imageReader1)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -122,6 +138,7 @@ func (g *Game) Init() {
 	g.freq = 10
 	g.over = false
 	g.bubbleImage = ebiten.NewImageFromImage(image)
+	g.weedImage = ebiten.NewImageFromImage(img)
 	
 }
 
@@ -129,7 +146,7 @@ func (g *Game) Drop() {
 	g.freq = 1
 }
 
-var colors []color.RGBA	= []color.RGBA{
+var colors []color.RGBA	= []color.RGBA{	
 	{5, 5, 5, 255},
 	{255, 0, 0, 255},
 	{0, 255, 0, 255},
@@ -156,6 +173,7 @@ func (g *Game) Draw (screen *ebiten.Image) {
 	if g.over {
 		
 	} else if g.block == nil {
+
 		g.block = &blocks[rand.Intn(7)]
 		g.blockX = 0
  		g.blockY = g.field.height - g.block.height - 1 
@@ -172,20 +190,26 @@ func (g *Game) Draw (screen *ebiten.Image) {
 	}
 	
 	options := &ebiten.DrawImageOptions {}
-	screen.Fill(color.RGBA64{255, 0, 255, 255})
-	bgImage := ebiten.NewImage(10, 10)
-	bgImage.Fill(colors[0])
+	screen.Fill(colors[0])
 
-	// Draw glass
+	options.GeoM.Translate(0, 60)
+	i := (g.counter / 10) % 15
+	sx, sy := frameOX+i*frameWidth, frameOY
+	screen.DrawImage(g.weedImage.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image), options)
+	options.GeoM.Translate(20, 0)
+	i += 5
+	sx, sy = frameOX+i*frameWidth, frameOY
+	screen.DrawImage(g.weedImage.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image), options)
+
+
+//	Draw glass
 	for i:=0; i < g.field.width; i++ {
 		for j := 0; j < g.field.height; j++ {
 			options.GeoM.Reset()
 			options.GeoM.Translate(float64(i*g.cell_size), float64(j*g.cell_size))
 			if (g.field.get(i,j) > 0) {
 				screen.DrawImage(g.bubbleImage, options)
-			} else {
-				screen.DrawImage(bgImage, options)
-			}			
+			} 			
 		}		
 	}
 
@@ -208,6 +232,12 @@ func (g *Game) Draw (screen *ebiten.Image) {
 		g.MergeBlock()
 		g.RemoveLines()
 	}
+
+
+	weedOptions := &ebiten.DrawImageOptions {}
+	weedOptions.GeoM.Translate(100, 100)
+	screen.DrawImage(g.weedImage, weedOptions)	
+	screen.DrawImage(g.bubbleImage, weedOptions)	
 
 }
 
@@ -263,6 +293,63 @@ func (g *Game) RemoveLines() {
 }
 
 func (g *Game) Update () error {
+
+	ids := inpututil.AppendJustPressedTouchIDs(nil)
+	if ids != nil {
+		for i := 0; i < len(ids); i++ {
+			b := inpututil.IsTouchJustReleased(ids[i])
+			z := inpututil.TouchPressDuration(ids[i])
+			x, y := ebiten.TouchPosition(ids[i])
+			fmt.Printf("Touch id = %d pos (%d, %d) dur %d released %t\n", ids[i], x, y, z, b)
+			if (x < 50 && y > 50) {
+
+				g.blockX -= 1
+				for i := 0; i < g.block.width; i++ {
+					for j := 0; j < g.block.height; j++ {
+						couldClashLeftSide := g.block.get(i, j) > 0 && (g.blockX + i) < 0
+						couldClashLeftBlock := g.block.get(i, j) > 0 && g.field.get(g.blockX + i, g.blockY + j) > 0;
+						if (couldClashLeftSide || couldClashLeftBlock) {
+							g.blockX += 1
+							return nil;
+						}
+					}
+				}
+
+			} else if (x > 50 && y > 50) {
+
+				g.blockX += 1
+				for i := 0; i < g.block.width; i++ {
+					for j := 0; j < g.block.height; j++ {
+						couldClashRightSide := g.block.get(i, j) > 0 && (g.blockX + i + 1) > g.field.width
+						couldClashRightBlock := g.block.get(i, j) > 0 && g.field.get(g.blockX + i, g.blockY + j) > 0;
+						if (couldClashRightSide || couldClashRightBlock) {
+							g.blockX -= 1
+							return nil;
+						}
+					}
+				}	
+
+			} else if (y < 50) {
+
+				g.block = g.block.RotateRight()
+				for i := 0; i < g.block.width; i++ {
+					for j := 0; j < g.block.height; j++ {
+						couldClashBlock := g.block.get(i, j) > 0 && g.field.get(g.blockX + i, g.blockY + j) > 0;
+						couldClashRightSide := g.block.get(i, j) > 0 && (g.blockX + i + 1) > g.field.width
+						couldClashLeftSide := g.block.get(i, j) > 0 && (g.blockX + i) < 0
+						if (!couldClashRightSide || !couldClashBlock || !couldClashLeftSide) {
+							fmt.Println("Rotation failed")
+							g.block = g.block.RotateLeft()
+							return nil;
+						}
+					}
+				}
+
+			}
+		}
+	}
+
+
 	if inpututil.IsKeyJustPressed(ebiten.KeyI) {
 		g.block.debug()
 	}
